9a21a0cbaab1fdf67c6b60ae0a417111083aa505
Karnow11 <andresignaciobc@gmail.com>
2023-10-31 19:22:02 -0300

    BucketSort & RadixSort changes
    Se cambió el bucket sort para que dado un array de ParPunteroYBits ordenara los bits que contienen esas estructuras devolviendo un array de punteros los cuales eran la representacion original de esos bits, además de Cambiar el Radix para que supiera trabajar con ese output

--- RadixSort.c
+++ RadixSort.c
@@ -14,9 +14,10 @@ unsigned long long* RadixSort(int* aOrdenar, int u, int k){
     Mask = ~Mask; // 1111111111111111111111111111111
     Mask << k; //11111111111111111111111110000 (k 0's)
     Mask = ~Mask; // 0000000000000000000000001111 (k 1's)
-    int TamañoArray = sizeof(aOrdenar)/sizeof(int);
+    int TamañoArray = sizeof(aOrdenar)/sizeof(int); // Tamaño array puede que sea necesario declararlo desde antes
     ParPunteroYBits** ArrayOrdenador = (int**)malloc(sizeof(ParPunteroYBits*)*TamañoArray);
-    int* ArrayOrdenador2 = malloc(sizeof(aOrdenar)*TamañoArray);
+    int* ArrayOrdenador2 = malloc(sizeof(int)*TamañoArray);
+    int* ArrayOrdenador3 = malloc(sizeof(int)*TamañoArray);
     int BitsPorRecorrer = sizeof(int)*8;
     int BitsRecorridos = k;
     while(BitsPorRecorrer > k){
@@ -26,9 +27,9 @@ unsigned long long* RadixSort(int* aOrdenar, int u, int k){
             j -> bits = aOrdenar[i]&&Mask;
             ArrayOrdenador[i] = j;
         }
-        ArrayOrdenador = BucketSort(ArrayOrdenador,u);
+        ArrayOrdenador2 = BucketSort(ArrayOrdenador,k);
         for(int j = 0 ;j < TamañoArray ;j++ ){
-            ArrayOrdenador2[j] = aOrdenar[ArrayOrdenador[j]->ptr];
+            ArrayOrdenador3[j] = aOrdenar[ArrayOrdenador2[j]];
         } 
         // Mask:  00000000000000000000000000000 1111
         Mask << k; // 0000000000000000000000000 1111 0000
@@ -45,11 +46,11 @@ unsigned long long* RadixSort(int* aOrdenar, int u, int k){
         j -> bits = aOrdenar[i]&&Mask;
         ArrayOrdenador[i] = j;
     }
-    ArrayOrdenador = BucketSort(ArrayOrdenador,u);
+    ArrayOrdenador = BucketSort(ArrayOrdenador,BitsPorRecorrer);
     for(int j = 0 ;j < TamañoArray ;j++ ){
-        ArrayOrdenador2[j] = aOrdenar[ArrayOrdenador[j]->ptr];
+        ArrayOrdenador3[j] = aOrdenar[ArrayOrdenador2[j]];
     }
-    return ArrayOrdenador2; 
+    return ArrayOrdenador3; 
 
 
 
